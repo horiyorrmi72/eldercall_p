88eeae1170ec0da1e74bdeef601744da92b0aad8
yusuf <horiyorrmi72@gmail.com>
2024-01-16 12:01:48 +0100

    users model

---
+++ models/users.model.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
