cdd18e6e3407e64b9c83c955ab4ae930382a95c2
yusuf <horiyorrmi72@gmail.com>
2024-03-10 12:30:23 +0100

    update play endpoint

--- routes/index.js
+++ routes/index.js
@@ -32,7 +32,7 @@ router.get("/customlogs", callcontroller.getCustomCallLogs);
 
 // audio routes
 router.post("/upload", audiocontroller.uploadAsset);
-router.get("/play-audio", audiocontroller.getAudioLinkByCategory);
+router.get("/play-audio/:category", audiocontroller.getAudioLinkByCategory);
 
 // sid creator
 router.get("/getSid", sidcontroller.createService);
