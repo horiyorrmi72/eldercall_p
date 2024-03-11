15fedac1f353919d45993a0c1cd5fcfd4e49d125
yusuf <horiyorrmi72@gmail.com>
2024-03-11 10:52:38 +0100

    read message from template

--- controllers/appcontroller.js
+++ controllers/appcontroller.js
@@ -1,5 +1,22 @@
+const path = require("path");
+const fs = require("fs");
+
 const ElderAppHome = async (req, res) => {
-  res.status(200).send("<h1>ElderCall</h1>");
+  const templatePath = path.join(
+    __dirname,
+    "../views/templates/elderMessage.html"
+  );
+
+  // Read the contents of the HTML file
+  fs.readFile(templatePath, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading HTML file:", err);
+      res.status(500).send("Internal Server Error");
+      return;
+    }
+    // Send the HTML content to the client
+    res.status(200).send(data);
+  });
 };
 
 module.exports = {
