855c636f3722965e7aec31fb4c6a49485df2ef25
yusuf <horiyorrmi72@gmail.com>
2024-07-21 22:31:16 +0100

    config passport

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
 require("dotenv").config();
 const express = require("express");
+const passport = require("passport"); 
+const passportConfig = require("./config/passport");
 const path = require("path");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const logger = require("morgan");
 const router = require("./routes");
 const connectDb = require("./config/db");
-const twilio = require("twilio");
 const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// Middleware
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({ secret: process.env.secret, resave: false, saveUninitialized: false }));
 app.use(cors());
-// Mount the router at the '/routes' path
+
+// Initializing Passport
+app.use(passport.initialize());
+
+// Configure Passport
+passportConfig(passport);
 
 app.use("/", router);
 
-// connect to database
+// Connecting to the database
 connectDb();
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
 module.exports = app;
