bf23b69c1028a5db9a17998b6f64064fe845ae6e
yusuf <horiyorrmi72@gmail.com>
2024-08-13 13:24:05 +0100

    disable eslint

--- config/db.js
+++ config/db.js
@@ -1,14 +1,15 @@
+/*eslint-disable*/
 require('dotenv').config();
 const mongoose = require('mongoose');
 const URL = process.env.db_url;
 
 const connectDb = async () => {
-	try {
-		await mongoose.connect(URL, { dbName: 'elderApp' });
-		console.log('Connected to the database successfully!');
-	} catch (err) {
-		console.error(`Error connecting to the database: ${err}`);
-	}
+  try {
+    await mongoose.connect(URL, { dbName: 'elderApp' });
+    console.log('Connected to the database successfully!');
+  } catch (err) {
+    console.error(`Error connecting to the database: ${err}`);
+  }
 };
 
 module.exports = connectDb;
