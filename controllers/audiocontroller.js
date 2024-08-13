d1dd227a708cc60344f21c042e6c2187ad2284a6
yusuf <horiyorrmi72@gmail.com>
2024-08-13 13:04:14 +0100

    using S3 bucket for the storage

--- controllers/audiocontroller.js
+++ controllers/audiocontroller.js
@@ -1,85 +1,153 @@
+require('dotenv').config();
 const twilio = require('twilio');
 const audio = require('../models/audio.model');
+const { S3Client } = require('@aws-sdk/client-s3');
+const { Upload } = require('@aws-sdk/lib-storage');
+const bucketName = process.env.BUCKET_NAME;
+
+const client = new S3Client({
+  credentials: {
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  },
+  region: 'eu-north-1',
+});
+
+const uploadDataToS3 = async (filename, bucket, fileBuffer) => {
+  try {
+    const data = new Upload({
+      client,
+      params: {
+        Bucket: bucket,
+        Key: filename,
+        Body: fileBuffer,
+        ContentType: 'audio/mpeg',
+        ACL: 'public-read',
+      },
+    });
+    const result = await data.done();
+    return result;
+  } catch (error)
+  {
+    /* eslint-disable-next-line no-console */
+    console.error('Error uploading file:', error);
+    throw error;
+  }
+};
 
 // handling file data and creating asset
 /**
  * Uploads an audio asset.
+ * uploads audio to S3 bucket
+ * return the location [URL] of the audio
+ * use it as the asset link and save it to the audio model
  *
  * @param {Object} req - Express request object
  * @param {Object} res - Express response object
- * @param {Function} next - Express next middleware function
- * @argument friendlyName - Name of the audio file
- * @argument category - category the audio file belongs to
- * @argument assetLink - audio file url generated.
+ * @param {Function} next - Express next middleware function if need be (optional)
+ * @argument friendlyName - Name of the audio file (keep it precise and unique)
+ * @argument category - category the audio file belongs to (['birthday', 'house-warming', 'wedding', 'naming', 'others']), this is base on the model object
+ * @argument assetLink - audio file url generated. this is returned after the audio file has been uploaded to the S3 Bucket on AWS
  *
  * @returns {Promise}
  */
-const uploadAsset = async (req, res, next) => {
-	const { friendlyName, category, assetLink } = req.body;
-	try {
-		if (!friendlyName || !category || !assetLink) {
-			return res.status(400).json({ error: 'All inputs required' });
-		}
+const uploadAsset = async (req, res) => {
+  const { friendlyName, category } = req.body;
+  const file = req.file;
 
-		const existingAsset = await audio.findOne({
-			$or: [{ friendlyName }, { assetLink }],
-		});
-		if (existingAsset) {
-			return res.status(409).json({
-				error: 'File with this data already exists',
-			});
-		}
-		const asset = new audio({
-			friendlyName,
-			category,
-			assetLink,
-		});
+  if (!file) {
+    return res.status(404).json({
+      success: false,
+      message:
+				'No file uploaded. Ensure you have selected an audio file to upload.',
+    });
+  }
 
-		await asset.save();
+  if (file.mimetype !== 'audio/mpeg') {
+    return res.status(400).json({
+      success: false,
+      message:
+				'File format not supported! Make sure to upload a file of the supported format.',
+    });
+  }
 
-		return res
-			.status(200)
-			.json({ message: 'Asset created successfully', asset, assetLink });
-	} catch (error) {
-		console.error('Error creating asset:', error);
-		res.status(500).json({ error: `Error creating asset: ${error.message}` });
-	}
+  if (!friendlyName || !category) {
+    return res.status(400).json({ error: 'All inputs required' });
+  }
+  let assetLink;
+  try {
+    const existingAsset = await audio.findOne({
+      $or: [{ friendlyName }, { assetLink }],
+    });
+
+    if (existingAsset) {
+      return res.status(409).json({
+        error: 'File with this data already exists',
+      });
+    }
+
+    const uniqueSuffix = `${Date.now()}_${'byAdmin'}`+ Math.round(Math.random() * 1E10);
+    const newName = `eldercall-${uniqueSuffix}-${file.originalname}`;
+
+    const uploadedData = await uploadDataToS3(newName, bucketName, file.buffer);
+
+    const asset = new audio({
+      friendlyName,
+      category,
+      assetLink: uploadedData.Location,
+    });
+
+    await asset.save();
+
+    res.status(200).json({
+      success: true,
+      message: 'Asset created successfully',
+      asset,
+      assetLink: uploadedData.Location,
+    });
+  } catch (error) {
+    // console.error('Error uploading asset:', error);
+    res.status(400).json({
+      success: false,
+      error: error.message,
+    });
+  }
 };
 
 /**
  * Fetches a random audio file link by category.
  *
  * @param {string} category - The audio category to fetch a random file from
- * @returns {Promise<string>} The TwiML markup for the random audio file
+ * //  * @returns {Promise<string>} The TwiML markup for the random audio file
  * @throws {Error} If no audio files found for the category
  */
-const getAudioLinkByCategory = async (req, res) => {
-	try {
-		const { category } = req.params;
-		const audioFiles = await audio.find({ category });
+const getAudioLinkByCategory = async (category) => {
+  try {
+    const audioFiles = await audio.find(category);
 
-		if (audioFiles.length === 0) {
-			throw new Error(`No audio files found for category: ${category}`);
-		}
+    if (audioFiles.length === 0) {
+      throw new Error(`No audio files found for category: ${category}`);
+    }
+    const randomIndex = Math.floor(Math.random() * audioFiles.length);
+    const randomAudioFile = audioFiles[randomIndex];
+    const result = randomAudioFile.assetLink;
+    // console.log(result);
+    return result.toString();
 
-		const randomIndex = Math.floor(Math.random() * audioFiles.length);
-		const randomAudioFile = audioFiles[randomIndex];
+    // const twiml = new twilio.twiml.VoiceResponse();
+    // twiml.play(randomAudioFile.assetLink, { loop: 1 });
 
-		const twiml = new twilio.twiml.VoiceResponse();
-		twiml.play(randomAudioFile.assetLink, { loop: 1 });
-
-		// Send the TwiML as response
-		res.type('text/xml');
-		res.send(twiml.toString());
-	} catch (error) {
-		console.error('Error fetching audio files:', error);
-		res
-			.status(500)
-			.json({ error: `Error fetching audio files: ${error.message}` });
-	}
+    // Send the TwiML as response
+    // res.type('text/xml');
+    // res.send(twiml.toString());
+  } catch (error)
+  {
+    /* eslint-disable-next-line no-console */
+    console.log({status:500, error: `Error fetching audio files: ${error.message}` });
+  }
 };
 
 module.exports = {
-	uploadAsset,
-	getAudioLinkByCategory,
+  uploadAsset,
+  getAudioLinkByCategory,
 };
