c1dacad8a70cccde2528812e90ae0b52424c2cce
yusuf <horiyorrmi72@gmail.com>
2024-08-20 14:57:22 +0100

    fixing call ish

--- controllers/callcontroller.js
+++ controllers/callcontroller.js
@@ -14,18 +14,18 @@ const callerId = 'elder_call';
 const defaultIdentity = callerId;
 
 const callAccessToken = (user) => {
-	const accessToken = new AccessToken(
-		process.env.TWILIO_ACCOUNT_SID,
-		process.env.API_KEY,
-		process.env.API_KEY_SECRET
-	);
-	accessToken.identity = defaultIdentity;
+  const accessToken = new AccessToken(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.API_KEY,
+    process.env.API_KEY_SECRET
+  );
+  accessToken.identity = defaultIdentity;
 
-	const voiceGrant = new AccessToken.VoiceGrant({
-		outgoingApplicationSid: 'to b added',
-	});
-	accessToken.addGrant(voiceGrant);
-	return accessToken.toJwt();
+  const voiceGrant = new AccessToken.VoiceGrant({
+    outgoingApplicationSid: 'to b added',
+  });
+  accessToken.addGrant(voiceGrant);
+  return accessToken.toJwt();
 };
 
 /**
@@ -41,185 +41,188 @@ const callAccessToken = (user) => {
  */
 
 const makeCall = async (req, res) => {
-	const user = req.user;
-	// console.log("Authenticated user:", user);
-	if (!user) {
-		// console.error('User object is not available, you must be logged in to use this feature');
-		return res
-			.status(500)
-			.send('User data is missing or incomplete, please try again');
-	}
+  const user = req.user;
+  // console.log("Authenticated user:", user);
+  if (!user) {
+    // console.error('User object is not available, you must be logged in to use this feature');
+    return res
+      .status(500)
+      .send('User data is missing or incomplete, please try again');
+  }
 
-	if (!user.phone) {
-		// console.error('User phone number is not available:', user.phone);
-		return res.status(500).send('User phone number is missing');
-	}
+  if (!user.phone) {
+    // console.error('User phone number is not available:', user.phone);
+    return res.status(500).send('User phone number is missing');
+  }
 
-	// Retrieve request body parameters
-	const { calleeNumber, calleeName, calldirection, audioCategory } = req.body;
+  // Retrieve request body parameters
+  const { calleeNumber, calleeName, calldirection, audioCategory } = req.body;
 
-	try {
-		if (!calleeNumber) {
-			// console.error('Recipient number is required but not provided');
-			return res.status(400).json({
-				msg: 'Recipient number is required',
-				data: {
-					call_direction: 'outbound',
-					success: false,
-					error: true,
-					error_msg: 'Recipient number is required',
-				},
-			});
-		}
+  try {
+    if (!calleeNumber) {
+      // console.error('Recipient number is required but not provided');
+      return res.status(400).json({
+        msg: 'Recipient number is required',
+        data: {
+          call_direction: 'outbound',
+          success: false,
+          error: true,
+          error_msg: 'Recipient number is required',
+        },
+      });
+    }
 
-		// console.log("Callee number:", calleeNumber);
+    // console.log("Callee number:", calleeNumber);
 
-		const twimlResponse = generateCallTwiML(calleeNumber, audioCategory, user);
-		// console.log("Generated TwiML Response:", twimlResponse);
+    const twimlResponse = generateCallTwiML(calleeNumber, audioCategory, user);
+    // console.log("Generated TwiML Response:", twimlResponse);
 
-		// Initiate the call
-		const call = await client.calls.create({
-			twiml: twimlResponse,
-			to: calleeNumber,
-			from: process.env.PHONE_NUMBER,
-			statusCallback: 'https://drab-zebu-6611.twil.io/status', //statusCallback url to be updated later
-			statusCallbackMethod: 'POST',
-			statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed'],
-		});
+    // Initiate the call
+    const call = await client.calls.create({
+      twiml: twimlResponse,
+      to: calleeNumber,
+      from: process.env.PHONE_NUMBER,
+      statusCallback: 'https://drab-zebu-6611.twil.io/status', //statusCallback url to be updated later
+      statusCallbackMethod: 'POST',
+      statusCallbackEvent: ['initiated', 'ringing', 'answered', 'completed'],
+    });
 
-		// console.log("Call object:", call);
+    // console.log("Call object:", call);
 
-		// Determine call direction
-		const callDirection =
+    // Determine call direction
+    const callDirection =
 			call.direction === 'outbound-api' ? 'outbound' : 'inbound';
 
-		// Create a call record
-		const callRecord = new Call({
-			userId: user._id,
-			callSid: call.sid,
-			phoneNumber: calleeNumber,
-			calldirection: calldirection || callDirection,
-			callDuration: call.duration || 0,
-			callStatus: call.status,
-			callDate: new Date().toDateString(),
-		});
+    // Create a call record
+    const callRecord = new Call({
+      userId: user._id,
+      callSid: call.sid,
+      phoneNumber: calleeNumber,
+      calldirection: calldirection || callDirection,
+      callDuration: call.duration || 0,
+      callStatus: call.status,
+      callDate: new Date().toDateString(),
+    });
 
-		await callRecord.save();
+    await callRecord.save();
 
-		res.status(200).json({
-			message: 'Call initiated successfully',
-			callRecord: callRecord,
-		});
-	} catch (err) {
-		// console.error('Error making the call:', err);
-		res.status(500).send('Failed to make the call');
-	}
+    res.status(200).json({
+      message: 'Call initiated successfully',
+      callRecord: callRecord,
+    });
+  } catch (err) {
+    // console.error('Error making the call:', err);
+    res.status(500).send('Failed to make the call');
+  }
 };
 
 const generateCallTwiML = async (calleeNumber, audioCategory, user) => {
-	const twiml = new VoiceResponse();
-	const audioToPlay = await getAudioLinkByCategory(audioCategory); // Pass string instead of object
-	twiml.play(`${audioToPlay}`);
-	twiml.say('A moment please while we connect you to the caller');
+  // console.log("User phone number in generateCallTwiML:", user.phone);
+  const twiml = new VoiceResponse();
+  const audioToPlay = await getAudioLinkByCategory(audioCategory);
+  twiml.play(`${audioToPlay}`);
+  twiml.say('A moment please while we connect you to the caller');
 
-	if (user.phone) {
-		twiml.dial(user.phone);
-	} else {
-		console.error('User phone number is missing when generating TwiML');
-	}
+  if (user.phone) {
+    twiml.dial(user.phone);
+  } else
+  {
+    /* eslint-disable-next-line no-console */
+    console.error('User phone number is missing when generating TwiML');
+  }
 
-	return twiml.toString();
+  return twiml.toString();
 };
 
 const getCurrentCallSid = async () => {
-	try {
-		const calls = await client.calls.list({
-			status: ['queued', 'in-progress', 'ringing'],
-		});
+  try {
+    const calls = await client.calls.list({
+      status: ['queued', 'in-progress', 'ringing'],
+    });
 
-		// console.log('Retrieved calls:', calls);
+    // console.log('Retrieved calls:', calls);
 
-		// Return an array of call SIDs
-		return calls.map((call) => call.sid);
-	} catch (error) {
-		// console.error('Failed to fetch current call SIDs:', error);
-		throw new Error('Failed to fetch current call SIDs');
-	}
+    // Return an array of call SIDs
+    return calls.map((call) => call.sid);
+  } catch (error) {
+    // console.error('Failed to fetch current call SIDs:', error);
+    throw new Error('Failed to fetch current call SIDs');
+  }
 };
 
 const endCall = async (req, res) => {
-	const user = req.user;
+  const user = req.user;
 
-	if (!user) {
-		return res.status(401).json({ message: 'User not authenticated' });
-	}
+  if (!user) {
+    return res.status(401).json({ message: 'User not authenticated' });
+  }
 
-	try {
-		// Fetch ongoing call SIDs
-		const callSids = await getCurrentCallSid();
+  try {
+    // Fetch ongoing call SIDs
+    const callSids = await getCurrentCallSid();
 
-		if (!callSids || callSids.length === 0) {
-			return res.status(404).json({ message: 'No ongoing calls found' });
-		}
+    if (!callSids || callSids.length === 0) {
+      return res.status(404).json({ message: 'No ongoing calls found' });
+    }
 
-		// Query for ongoing calls associated with the user
-		const currentUserCall = await Call.findOne({
-			userId: user._id,
-			callSid: { $in: callSids },
-			callStatus: ['in-progress', 'queued', 'ringing'],
-		});
+    // Query for ongoing calls associated with the user
+    const currentUserCall = await Call.findOne({
+      userId: user._id,
+      callSid: { $in: callSids },
+      callStatus: ['in-progress', 'queued', 'ringing'],
+    });
 
-		if (currentUserCall) {
-			// End the call
-			await client
-				.calls(currentUserCall.callSid)
-				.update({ status: 'completed' });
-			return res.status(200).json({ message: 'Call ended successfully' });
-		} else {
-			return res
-				.status(404)
-				.json({ message: 'No ongoing calls found for this user' });
-		}
-	} catch (error) {
-		// console.error('Failed to end the call:', error);
-		return res.status(500).json({ message: 'Failed to end call', error });
-	}
+    if (currentUserCall) {
+      // End the call
+      await client
+        .calls(currentUserCall.callSid)
+        .update({ status: 'completed' });
+      return res.status(200).json({ message: 'Call ended successfully' });
+    } else {
+      return res
+        .status(404)
+        .json({ message: 'No ongoing calls found for this user' });
+    }
+  } catch (error) {
+    // console.error('Failed to end the call:', error);
+    return res.status(500).json({ message: 'Failed to end call', error });
+  }
 };
 
 const webhook = async (req, res) => {
-	try {
-		const status = req.body.CallStatus;
-		const callSid = req.body.CallSid;
+  try {
+    const status = req.body.CallStatus;
+    const callSid = req.body.CallSid;
 
-		console.log(`Call status update: ${status}, Call SID: ${callSid}`);
+    console.log(`Call status update: ${status}, Call SID: ${callSid}`);
 
-		if (status === 'completed') {
-			client
-				.calls(callSid)
-				.fetch()
-				.then((call) => {
-					const audioUrl = call.url;
-					console.log(`Audio file URL: ${audioUrl}`);
-				})
-				.catch((error) => {
-					console.error('Failed to fetch call details:', error);
-				});
-		}
+    if (status === 'completed') {
+      client
+        .calls(callSid)
+        .fetch()
+        .then((call) => {
+          const audioUrl = call.url;
+          console.log(`Audio file URL: ${audioUrl}`);
+        })
+        .catch((error) => {
+          console.error('Failed to fetch call details:', error);
+        });
+    }
 
-		res.status(200).end();
-	} catch (error) {
-		// console.error('Error handling webhook:', error);
-		res.status(500).send('Internal Server Error');
-	}
+    res.status(200).end();
+  } catch (error) {
+    // console.error('Error handling webhook:', error);
+    res.status(500).send('Internal Server Error');
+  }
 };
 const getTwilioCallLogs = async (req, res) => {
-	try {
-		const calls = await client.calls.list({ limit: 20 });
-		res.status(200).json({ calls });
-	} catch (error) {
-		// console.error('Failed to fetch call logs:', error);
-		res.status(500).json({ error: 'Failed to fetch call logs' });
-	}
+  try {
+    const calls = await client.calls.list({ limit: 20 });
+    res.status(200).json({ calls });
+  } catch (error) {
+    // console.error('Failed to fetch call logs:', error);
+    res.status(500).json({ error: 'Failed to fetch call logs' });
+  }
 };
 
 /**
@@ -229,13 +232,13 @@ const getTwilioCallLogs = async (req, res) => {
  
 */
 const getCustomCallLogs = async (req, res) => {
-	try {
-		const callLogs = await Call.find();
-		res.status(200).json({ callLogs });
-	} catch (err) {
-		// console.error('Failed to fetch call logs:', err);
-		res.status(500).json({ error: 'Failed to fetch call logs' });
-	}
+  try {
+    const callLogs = await Call.find();
+    res.status(200).json({ callLogs });
+  } catch (err) {
+    // console.error('Failed to fetch call logs:', err);
+    res.status(500).json({ error: 'Failed to fetch call logs' });
+  }
 };
 
 /**
@@ -247,25 +250,25 @@ const getCustomCallLogs = async (req, res) => {
  */
 
 const getCustomOutboundCallLogs = async (req, res) => {
-	try {
-		// const user = req.user._id;
-		const callsData = await Call.find({
-			calldirection: (calldirection = 'outbound'),
-		});
-		// console.log(callsData);
-		res.status(200).json({ data: callsData });
-	} catch (error) {
-		// console.error('Failed to fetch call logs:', error);
-		res.status(500).json({ error: 'Failed to fetch call logs' });
-	}
+  try {
+    // const user = req.user._id;
+    const callsData = await Call.find({
+      calldirection: (calldirection = 'outbound'),
+    });
+    // console.log(callsData);
+    res.status(200).json({ data: callsData });
+  } catch (error) {
+    // console.error('Failed to fetch call logs:', error);
+    res.status(500).json({ error: 'Failed to fetch call logs' });
+  }
 };
 
 module.exports = {
-	makeCall,
-	endCall,
-	getCurrentCallSid,
-	webhook,
-	getTwilioCallLogs,
-	getCustomOutboundCallLogs,
-	getCustomCallLogs,
+  makeCall,
+  endCall,
+  getCurrentCallSid,
+  webhook,
+  getTwilioCallLogs,
+  getCustomOutboundCallLogs,
+  getCustomCallLogs,
 };
