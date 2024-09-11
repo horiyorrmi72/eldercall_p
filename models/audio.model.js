c7dc42661ae018c77c0652618e0575b17918782f
yusuf <horiyorrmi72@gmail.com>
2024-09-11 15:36:49 +0100

    added updtates to the audio enums

--- models/audio.model.js
+++ models/audio.model.js
@@ -2,22 +2,37 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const audioSchema = new Schema({
-  friendlyName: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    enum: ['birthday', 'house-warming', 'wedding', 'naming', 'others'],
-    required: true,
-  },
-  assetLink: {
-    type: String,
-  },
-  uploadDate: {
-    type: Date,
-    default:Date.now(),
-  },
+	friendlyName: {
+		type: String,
+		required: true,
+	},
+	category: {
+		type: String,
+		enum: [
+			'birthday',
+			'house-warming',
+			'wedding',
+			'naming',
+			'love',
+			'christmas',
+			'new-year',
+			'easter',
+			'prayer',
+			'sallah',
+			'others',
+		],
+		required: true,
+	},
+	assetLink: {
+		type: String,
+	},
+	shortVersionAssetLink: {
+		type: String,
+	},
+	uploadDate: {
+		type: Date,
+		default: Date.now(),
+	},
 });
 
 const audio = mongoose.model('audios', audioSchema);
